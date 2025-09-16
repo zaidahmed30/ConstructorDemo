62c910b18d0f057d2166452efcaa417397b31c57
Zaid Ahmed <ahmedzaid219@outlook.com>
2025-09-16 23:47:35 +0530

    todays update

---
+++ ConstructorDemo/ConstructorsDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo
+{
+    public class ConstructorsDemo
+    {
+        static int y;
+        int x;
+        static ConstructorsDemo()
+        {
+            Console.WriteLine("Static constructor is called.");
+        }
+        public ConstructorsDemo(int x)
+        {
+            this.x = x;
+            Console.WriteLine("Non-Static constructor is called.");
+
+        }
+        static void Main()
+        {
+            ConstructorsDemo cd1 = new ConstructorsDemo(10);
+            ConstructorsDemo cd2 = new ConstructorsDemo(20);
+            ConstructorsDemo cd3 = new ConstructorsDemo(30);
+
+            Console.WriteLine(y);
+            Console.WriteLine(cd1.x + " " + cd2.x + " " + cd3.x);
+        }
+    }
+}
--- ConstructorDemo/StaticConDemo.cs
+++ ConstructorDemo/StaticConDemo.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConstructorDemo
-{
-    public class StaticConDemo
-    {
-        static StaticConDemo()
-        {
-            Console.WriteLine("Static constructor is executed.");
-        }
-        static void Main()
-        {
+//namespace ConstructorDemo
+//{
+//    public class StaticConDemo
+//    {
+//        static StaticConDemo()
+//        {
+//            Console.WriteLine("Static constructor is executed.");
+//        }
+//        static void Main()
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
